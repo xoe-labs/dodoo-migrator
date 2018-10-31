8619fca1dfeaaf591252a41899337830cd4a31b5
David Arnold <dar@xoe.solutions>
2018-10-31 16:35:18 -0500

    [REF] black+isort run

--- setup.py
+++ setup.py
@@ -2,43 +2,41 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 import os
-from setuptools import setup, find_packages
 
+from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name='click-odoo-{{ PROJECT }}',
-    description='click-odoo {{ PROJECT }} script',
-    long_description='\n'.join((
-        open(os.path.join(here, 'README.rst')).read(),
-        open(os.path.join(here, 'CHANGES.rst')).read(),
-    )),
+    name="click-odoo-{{ PROJECT }}",
+    description="click-odoo {{ PROJECT }} script",
+    long_description="\n".join(
+        (
+            open(os.path.join(here, "README.rst")).read(),
+            open(os.path.join(here, "CHANGES.rst")).read(),
+        )
+    ),
     use_scm_version=True,
     packages=find_packages(),
-    setup_requires=[
-        'setuptools-scm',
-    ],
-    install_requires=[
-        'click-odoo>=1.0.0',
-    ],
-    license='LGPLv3+',
-    author='{{ PACKAGE_AUTHOR }}',
-    author_email='{{ PACKAGE_AUTHOR_EMAIL }}',
-    url='http://github.com/{{ GITHUBORG }}/click-odoo-{{ PROJECT }}',
+    setup_requires=["setuptools-scm"],
+    install_requires=["click-odoo>=1.0.0"],
+    license="LGPLv3+",
+    author="{{ PACKAGE_AUTHOR }}",
+    author_email="{{ PACKAGE_AUTHOR_EMAIL }}",
+    url="http://github.com/{{ GITHUBORG }}/click-odoo-{{ PROJECT }}",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: '
-        'GNU Lesser General Public License v3 or later (LGPLv3+)',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Framework :: Odoo',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        "GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: Odoo",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         click-odoo-{{ PROJECT }}=src.{{ PROJECT }}:main
-    ''',
+    """,
 )
